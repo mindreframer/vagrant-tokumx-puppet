7d3fc273ab6930ea548590b2b87c727a07d0d05d
Roman Heinrich <roman.heinrich@gmail.com>
2013-08-13 20:38:51 +0200

    hey, a bug! string interpolation

--- puppet/modules/tokumx/manifests/params.pp
+++ puppet/modules/tokumx/manifests/params.pp
@@ -3,7 +3,7 @@ class tokumx::params{
   $download_file = "$fullpath.tgz"
   $packagenames  = [] #["libaio1", "mysql-client-core-5.5"] # ubuntu 12.04
   # a tmp location
-  $download_url  = "http://master.dl.sourceforge.net/project/tokumxdownload/#{download_file}"
+  $download_url  = "http://master.dl.sourceforge.net/project/tokumxdownload/${download_file}"
   # also group_id, for consistency
   $user_id       = 915
   $base_dir      = '/usr/local/tokumx'
